1931ffda720b1ae2043b9c74e5724b53393565c4
tomomi <user@centos.example.com>
2020-07-16 11:51:10 +0900

    add a function to judge if the number is match to the number of files in the current directory or not

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+function isitmatch {
+	ans=`ls | wc -l`
+	if [[ $1 -eq $ans ]] ; then
+		echo "Congrats!"
+	elif [[ $1 -gt $ans ]] ; then
+		echo "Incorrect. Please try small number."
+	else 
+		echo "Incorrect. Please try big number."
+	fi	
+}
+
+
 echo "Quiz:How many files are in the current directory?"
 read response
 
 if expr "$response" : '[0-9]*' > /dev/null ; then
-	echo "number"	
+	isitmatch $response	
 else
 	echo "please input a numerical value"
 fi
-
