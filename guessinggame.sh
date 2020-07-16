ec492307ea6d23026723d12e6868090cfbab83fd
tomomi <user@centos.example.com>
2020-07-16 10:26:53 +0900

    add numerical judgement

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,4 +3,9 @@
 echo "Quiz:How many files are in the current directory?"
 read response
 
-echo $response
+if expr "$response" : '[0-9]*' > /dev/null ; then
+	echo "number"	
+else
+	echo "please input a numerical value"
+fi
+
