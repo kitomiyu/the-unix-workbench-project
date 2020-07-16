33285de0b855b87765f76eb3a3d40ba4ed172c32
user <user@centos.example.com>
2020-07-16 09:46:18 +0900

    first commit

---
+++ guessinggame.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Quiz:How many files are in the current directory?"
+read response
+
+echo $response
---
+++ makefile
@@ -0,0 +1,2 @@
+README.md: guessinggame.sh
+	wc -l giessinggame.sh | egrep -o "[0-9]+" >> README.md 
