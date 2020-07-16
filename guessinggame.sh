8105716f05a2edc53da56193f7725f68786ff710
tomomi <user@centos.example.com>
2020-07-16 20:08:00 +0900

    add a loop to guess the number when it's incorrect

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+flag=0
+
 function isitmatch {
 	ans=`ls | wc -l`
 	if [[ $1 -eq $ans ]] ; then
 		echo "Congrats!"
+		flag=1
 	elif [[ $1 -gt $ans ]] ; then
 		echo "Incorrect. Please try small number."
 	else 
-		echo "Incorrect. Please try big number."
+		echo "Incorrect. Please try lerge  number."
 	fi	
 }
 
+while [[ flag -eq 0 ]]
+do
+	echo "Quiz:How many files are in the current directory?"
+	read response
 
-echo "Quiz:How many files are in the current directory?"
-read response
+	if expr "$response" : '[0-9]*' > /dev/null ; then
+		isitmatch $response	
+	else
+		echo "please input a numerical value"
+	fi
+done
 
-if expr "$response" : '[0-9]*' > /dev/null ; then
-	isitmatch $response	
-else
-	echo "please input a numerical value"
-fi
